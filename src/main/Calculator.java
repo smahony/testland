d02993a83f5ae4d0e962de4fc752bd49655f7595
Samantha <mahony.s.tutoring@gmail.com>
2013-11-18 21:11:13 -0800

    Created new addition and subtraction functions in Calculator class

---
+++ src/main/Calculator.java
@@ -0,0 +1,27 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: samanthamahony
+ * Date: 11/18/13
+ * Time: 8:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+    public int addition(int x, int y){
+
+        return x + y;
+    }
+
+    public int subtraction(int x, int y){
+
+        return x - y;
+    }
+}
